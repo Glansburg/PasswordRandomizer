6a3442057b263195f01c7ea753eabb0e28f0ad96
Joel Getzke <joelcode2023@gmail.com>
2023-05-14 15:57:30 -0500

    Arrays added

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,34 @@
 // Assignment Code
+
+
+function passwordGeneration() {
+  // created sets of arrays for all the array criteria for the project.
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+  var specialChars = ["!","@","#","$", "%","%","^", "&", "*", "(",")","-","+","?","/",":",";","[","}",".","<","<","=","_","'","|","~"];
+  var selectedArray= [];
+}
+
+var pwordLength = getPwordLength
+
+var charTypePicked = false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,5 +40,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button. goes to end
 generateBtn.addEventListener("click", writePassword);
