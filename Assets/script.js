87f513b9542a2d4763e284be6114273d67335db2
Joel Getzke <joelcode2023@gmail.com>
2023-05-18 12:11:15 -0500

    added code for lowercase letters

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,7 @@
 // variable targeting the big red "Generate Password button"
 var generateBtn = document.querySelector("#generate");
 
-//testing to see if button works
+//testing to see if button works with console.log
 function generatePassword() {
   console.log("hello this is a test")
   //setting up intial values
@@ -13,6 +13,9 @@ function generatePassword() {
   var digits = false
   var specialChars = false
 
+  // Setting up intial arrays of Data.
+  var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // 1. prompt the user for the password criteria
 passwordLength = prompt('How many characters would you like in your password? Choose "number 8 through 128"');
 
@@ -22,9 +25,26 @@ while(((passwordLength < 8) || (passwordLength > 128))) {
   passwordLength = prompt('How many characters would you like in your password? Choose "number 8 through 128"');
 }
 
+//START HERE ON GETTING LOWERCASE LETTERS OF USER ADDED IN DATA
+lowerCase = confirm('Would you like lowercase letters in your password?');
+if (lowerCase === true) {
+  lowerCase = lowerCaseChars;
+  console.log(lowerCase);
+} else {
+  lowerCase === false;
+  console.log(lowerCase);
+}
 
-confirm('Would you like lowercase letters in your password?');
+//Getting UpperCase letters of user added in data
 confirm("Would you like uppercase leters in your password?");
+
+
+
+
+
+
+
+
 confirm("Would you like digits in your password?");
 confirm("Would you like special characters in your password?");
 
